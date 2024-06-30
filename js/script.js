16fd59d7d8a4830a58c67acbdbdce712204d1d85
micheal2024 <2413605669@qq.com>
2024-06-30 20:17:19 +0800

    init2

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     $("#page-header").load("./subHtml/header.html",function (){
         // 核查哪些链接需要密码
         $(".needPasswordLink").click(function(){
-            var password = prompt("你知道她么?");
-            if(password == "chenqian"){
+            var password = prompt("我的小可爱是谁呀?");
+            if(password == "何姝"){
                 alert("密码校验成功");
                 var url = $(this).attr("realLink");
                 $(location).attr('href', url);
